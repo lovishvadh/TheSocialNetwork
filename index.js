183966a0bd3770ac64bf16823364a549309cc009
lovish <lovishthekingofkings@gmail.com>
2017-12-10 04:56:12 +0530

    removed useless colors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ $(document).ready(() => {
       web3.eth.defaultAccount=web3.eth.accounts[0];
       
       var snContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"i","type":"uint256"}],"name":"incrementLikes","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"writerActs","outputs":[{"name":"timestamp","type":"uint256"},{"name":"title","type":"string"},{"name":"content","type":"string"},{"name":"name","type":"string"},{"name":"writer","type":"address"},{"name":"likes","type":"uint256"},{"name":"dislikes","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"i","type":"uint256"}],"name":"getWriterActs","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"i","type":"uint256"}],"name":"incrementDislikes","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"i","type":"uint256"}],"name":"getdislikes","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"i","type":"uint256"}],"name":"getlikes","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"countPosts","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"title","type":"string"},{"name":"text","type":"string"},{"name":"name","type":"string"}],"name":"insertPost","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]);
-      var socialNetwork = snContract.at('0x49bf0c6b94831a9ff37ac4f4fbf65ebabaef310a');
+      var socialNetwork = snContract.at('0xc405b01c97c23bbe7a9c27e33a5628c01d53eeb9');
       var prevResponse = 0;
       //var writeracts = socialNetwork.getWriterActs(i);
       //var writeractsLen = writeracts.length;
@@ -39,7 +39,7 @@ $(document).ready(() => {
                 +res[0]+'</h3><br /></center><span id="content">'
                 +res[1]+'</span><br /><br /><span id="writerName">Post Curator:'
                 +res[2]+'</span><span id="time">Post Time:'
-                +res[3]+'</span><br /><span id= "upVotes">UPVOTES :'
+                +(new Date(res[3]*1000)).toUTCString()+'</span><br /><span id= "upVotes">UPVOTES :'
                 +res[4]+'</span><span id= "downVotes">DOWNVOTES :'
                 +res[5]+'</span><br />'
                 +'<button class="waves-effect waves-light btn upBtn" id="hello"><i class="material-icons left">thumb_up</i>UPVOTE</button>'
@@ -53,11 +53,13 @@ $(document).ready(() => {
       });
     $('body').on('click', 'button.upBtn', function() {
         var index = $(this).parent().attr('class');
-        socialNetwork.incrementLikes(index);       
+        socialNetwork.incrementLikes(index);    
+        $(this).addClass('disabled');   
     });
-    $('body').on('click', 'button.upBtn', function() {
+    $('body').on('click', 'button.dvBtn', function() {
         var index = $(this).parent().attr('class');
-        socialNetwork.incrementdisLikes(index);       
+        socialNetwork.incrementDislikes(index);  
+        $(this).addClass('disabled');       
     });
       setInterval(() => { socialNetwork.countPosts((err,response) => {
         var y = parseInt(response.c);
@@ -72,7 +74,7 @@ $(document).ready(() => {
                 +res[0]+'</h3><br /></center><span id="content">'
                 +res[1]+'</span><br /><br /><span id="writerName">Post Curator:'
                 +res[2]+'</span><span id="time">Post Time:'
-                +res[3]+'</span><br /><span id= "upVotes">UPVOTES :'
+                +(new Date(res[3]*1000)).toUTCString()+'</span><br /><span id= "upVotes">UPVOTES :'
                 +res[4]+'</span><span id= "downVotes">DOWNVOTES :'
                 +res[5]+'</span><br />'
                 +'<button class="waves-effect waves-light btn upBtn"><i class="material-icons left">thumb_up</i>UPVOTE</button>'

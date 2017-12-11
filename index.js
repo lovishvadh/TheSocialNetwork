30614366f56ceffd8d3bd24642c92876b928d141
lovish <lovishthekingofkings@gmail.com>
2017-12-11 22:54:53 +0530

    real time updates

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ $(document).ready(() => {
       web3.eth.defaultAccount=web3.eth.accounts[0];
       
       var snContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"i","type":"uint256"}],"name":"incrementLikes","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"writerActs","outputs":[{"name":"timestamp","type":"uint256"},{"name":"title","type":"string"},{"name":"content","type":"string"},{"name":"name","type":"string"},{"name":"writer","type":"address"},{"name":"likes","type":"uint256"},{"name":"dislikes","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"i","type":"uint256"}],"name":"getWriterActs","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"i","type":"uint256"}],"name":"incrementDislikes","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"i","type":"uint256"}],"name":"getdislikes","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"i","type":"uint256"}],"name":"getlikes","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"countPosts","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"title","type":"string"},{"name":"text","type":"string"},{"name":"name","type":"string"}],"name":"insertPost","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]);
-      var socialNetwork = snContract.at('0x0a3e6b43476a7391d4d76b7a1675f32414e2db89');
+      var socialNetwork = snContract.at('0xe2c4826abcf1f20ff94935d7472307a69b1cc89a');
       var prevResponse = 0;
       //var writeracts = socialNetwork.getWriterActs(i);
       //var writeractsLen = writeracts.length;
@@ -24,10 +24,12 @@ $(document).ready(() => {
             $("#postsul").append('<li id="post"><center><h3 id="title"></h3><br /></center><span id="content">'+res[2]+'</span><br /><br /><span id="writerName">'+res[1]+'</span><span id="time">'+res[0]+'</span><br /></li>'); 
           });
       }*/
-      
+      var likes = [];
+      var dislikes = [];
 
       socialNetwork.countPosts((err,response) => {
       if(response){
+        $('#loader').show();
         let count = 0;
         var z = parseInt(response.c);
         prevResponse = z;
@@ -37,29 +39,37 @@ $(document).ready(() => {
             socialNetwork.getWriterActs(i, (err,res) => {
                 $("#postsul").prepend('<li id="post" class="'+ count +'"><center><h3 id="title">'
                 +res[0]+'</h3><br /></center><span id="content">'
-                +res[1]+'</span><br /><br /><span id="writerName">Post Curator:'
+                +res[1]+'</span><br /><br /><span class="updiv"><span id= "upVotes">UpVotes:<span id="manLikes">'
+                +res[4]+'</span></span><button class="waves-effect waves-light btn upBtn" id="hello"><i class="material-icons">thumb_up</i></button></span>'
+                +'<span class="downdiv"><span id= "downVotes">DownVotes:<span id="manDislikes">'
+                +res[5]+'</span></span>'
+                +'<button class="waves-effect waves-light btn dvBtn"><i class="material-icons">thumb_down</i></button></span>'
+                +'<br /><br /><span id="writerName">Post Curator:'
                 +res[2]+'</span><span id="time">Post Time:'
-                +(new Date(res[3]*1000)).toUTCString()+'</span><br /><span id= "upVotes">UPVOTES :'
-                +res[4]+'</span><span id= "downVotes">DOWNVOTES :'
-                +res[5]+'</span><br />'
-                +'<button class="waves-effect waves-light btn upBtn" id="hello"><i class="material-icons left">thumb_up</i>UPVOTE</button>'
-                +'<button class="waves-effect waves-light btn dvBtn"><i class="material-icons left">thumb_down</i>DOWNVOTE</button>'
-                +'</li>');  
-            count = count + 1;          
+                +(new Date(res[3]*1000)).toUTCString()+'</span><br /></li>');       
+                likes[count] = parseInt(res[4]);
+                dislikes[count] = parseInt(res[5]);
+                count = count + 1;  
             });
             
           }
+          $('#loader').hide();
        }
       });
     $('body').on('click', 'button.upBtn', function() {
-        var index = $(this).parent().attr('class');
+        var index = $(this).parent().parent().attr('class');
         socialNetwork.incrementLikes(index);    
-        //$(this).addClass('disabled');   
+        likes[index] = likes[index] + 1;
+        let cin = likes[index];
+        $(this).siblings().children().html(cin);    
     });
     $('body').on('click', 'button.dvBtn', function() {
-        var index = $(this).parent().attr('class');
+        var index = $(this).parent().parent().attr('class');
         socialNetwork.incrementDislikes(index);
-        //$(this).addClass('disabled'); 
+        dislikes[index] = dislikes[index] + 1;
+        let cin = dislikes[index];
+        $(this).siblings().children().html(cin); 
+        
     });
       setInterval(() => { socialNetwork.countPosts((err,response) => {
         var y = parseInt(response.c);
@@ -70,16 +80,18 @@ $(document).ready(() => {
             $('#countPosts').html('Total Posts: '+ response.c);
                var x = response.c-1;
                socialNetwork.getWriterActs(x, (err,res) => {
-                $("#postsul").prepend('<li id="post" class="'+x+'"><center><h3 id="title">'
+                $("#postsul").prepend('<li id="post" class="'+ x +'"><center><h3 id="title">'
                 +res[0]+'</h3><br /></center><span id="content">'
-                +res[1]+'</span><br /><br /><span id="writerName">Post Curator:'
+                +res[1]+'</span><br /><br /><span class="updiv"><span id= "upVotes">UpVotes:'
+                +res[4]+'</span><button class="waves-effect waves-light btn upBtn" id="hello"><i class="material-icons">thumb_up</i></button></span>'
+                +'<span class="downdiv"><span id= "downVotes">DownVotes:'
+                +res[5]+'</span>'
+                +'<button class="waves-effect waves-light btn dvBtn"><i class="material-icons">thumb_down</i></button></span>'
+                +'<br /><br /><span id="writerName">Post Curator:'
                 +res[2]+'</span><span id="time">Post Time:'
-                +(new Date(res[3]*1000)).toUTCString()+'</span><br /><span id= "upVotes">UPVOTES :'
-                +res[4]+'</span><span id= "downVotes">DOWNVOTES :'
-                +res[5]+'</span><br />'
-                +'<button class="waves-effect waves-light btn upBtn"><i class="material-icons left">thumb_up</i>UPVOTE</button>'
-                +'<button class="waves-effect waves-light btn dvBtn"><i class="material-icons left">thumb_down</i>DOWNVOTE</button>'
-                +'</li>');        
+                +(new Date(res[3]*1000)).toUTCString()+'</span><br /></li>');    
+                likes[x] = parseInt(res[4]);
+                dislikes[x] = parseInt(res[5]);  
              });
             }
         })
